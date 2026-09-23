baef4da509302a6a9b238aeab39cc505564c9ba6
mokotanin <titchou68@gmail.com>
2026-09-23 18:39:04 +0200

    add: pkgs & components for website

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/nsi/",
+  resolve: {
+    alias: {
+      "@": path.resolve(import.meta.dirname, "./src"),
+    },
+  },
+});
